d33358677b8d304c1fa2a08c8dd2f2dc1aec9f3a
janmicko <mejia.johnmikkoi@gmail.com>
2024-11-19 07:24:54 -0800

    asdf

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ const checkDead = setInterval(function () {
 
     if (blockLeft < 70 && blockLeft > 50 && characterTop >= 150) {
         block.style.animation = "none";
-        alert("Game Over. Score: " + Math.floor(counter / 100));
         endSound.play();
+        alert("Game Over. Score: " + Math.floor(counter / 100));
         counter = 0;
         block.style.animation = "block 1.5s infinite linear";
     } else {
